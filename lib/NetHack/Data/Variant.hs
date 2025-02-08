8ebc988502229c69385451f874d5d1898a03c92f
Mikko Juola <mikjuo@gmail.com>
2025-02-07 18:59:14 -0800

    Support compressed, .yaml.gz files. Makes Slex ~20M -> 2M.
    Slex variant has unusually large number of monsters compared to all the
    other variants.
    
    As of writing this commit, slex is also disabled by default (unlikely
    it will be queried through Pinobot), but I wanted to preserve the .yaml
    file as a tangible file within the repository (and not just as an
    artifact in git commit history).
    
    Makes the source code checkout smaller.
    
    I also tested zstd first, and it actually made a bigger file than .gz. I
    chose .gz instead of modern .zst compression, because the zlib library
    in Haskell looks much more mature and unlikely to bit-rot.
    
    Pinobot automatically first tries to load .yaml file, and then, if it
    does not exist, it will try to load .yaml.gz.
    
    I kept other variant .yaml files uncompressed, so that they are easier
    to browse (in GitHub or in other UIs).

--- lib/NetHack/Data/Variant.hs
+++ lib/NetHack/Data/Variant.hs
@@ -2,6 +2,7 @@
 -- should implement. See `Variant`.
 --
 
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module NetHack.Data.Variant
@@ -13,13 +14,17 @@ module NetHack.Data.Variant
     , loadVariant )
     where
 
+import qualified Codec.Compression.GZip as GZip
 import Control.Applicative
+import Control.DeepSeq
 import Control.Monad.IO.Class
-import qualified Data.ByteString as B
+import Control.Exception
+import qualified Data.ByteString.Lazy as BL
 import Data.List ( find )
 import qualified Data.Text as T
 import Data.Yaml
 import qualified NetHack.Data.Monster as MD
+import System.IO.Error
 
 -- | Export a function that returns one of these to add a variant to the bot.
 -- See `variant`.
@@ -52,8 +57,28 @@ variant = Variant
 -- Loads a variant from a YAML file.
 loadVariant :: MonadIO m => FilePath -> m Variant
 loadVariant fpath = liftIO $ do
-    bs <- B.readFile fpath
-    case decodeEither bs of
-        Left err -> error err
-        Right var -> return var
+  maybe_bs <- try $ BL.readFile fpath
+  -- FIXME: this nested case is a mess but I am one lazy programmer
+  case maybe_bs of
+    Left (_err :: IOError) | isDoesNotExistError _err -> do
+      -- Try load the compressed version if plain version doesn't exist.
+      let fpath_gz = fpath <> ".gz"
+      maybe_compressed_bs <- try $ BL.readFile fpath_gz
+      case maybe_compressed_bs of
+        Left (_err :: IOError) | isDoesNotExistError _err ->
+          error $ "Cannot find either " <> fpath <> " or " <> fpath_gz
+        Left err -> throwIO err
+        Right compressed_bs -> do
+          let bs_lazy = GZip.decompress compressed_bs
+          -- GZip.decompress is lazy, force the bytestring so that we error out here
+          -- if the file itself is bad/corrupted/whatever.
+          bs <- evaluate $ force bs_lazy
+          goDecode bs
+    Left err -> throwIO err
+    Right bs -> goDecode bs
+ where
+  goDecode :: BL.ByteString -> IO Variant
+  goDecode bs = case decodeEither' (BL.toStrict bs) of
+    Left err -> error $ "Parsing variant .yaml failed: " <> show err
+    Right var -> return var
 
