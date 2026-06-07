cea55a66a89c015d39e81d7331bf05e15274d8c7
Mikko Juola <mikko.juola@iki.fi>
2026-06-07 12:15:59 -0700

    Format + fix warnings in lib/IRC/Types.hs
    Removed import for Data.Monoid, it was used for importing
    monoid/semigroup append (<>) operation, but that has been part of
    Prelude since GHC 8.0/base-4.9 (and pinobot is at least base-4.11 atm
    due to usage of T.foldlM' from Data.Text 2.1.2). This fixes a warning.
    
    Other than that, rest of this commit just formats the file according to
    my autoformatter.

--- lib/IRC/Types.hs
+++ lib/IRC/Types.hs
@@ -1,70 +1,77 @@
-{-# LANGUAGE DeriveGeneric, DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# OPTIONS_GHC -fno-warn-name-shadowing #-}
 
 module IRC.Types
-    ( IRCMessage(..)
-    , connectToBot
-    , receiveIRCMessages )
-    where
+  ( IRCMessage (..),
+    connectToBot,
+    receiveIRCMessages,
+  )
+where
 
-import qualified Data.Text as T
-import Data.Serialize
-import Data.Serialize.Text()
-import Data.Monoid
-import Data.Typeable
-import Data.Word
-import Data.IORef
-import GHC.Generics
-import Control.Monad
-import Control.Concurrent hiding ( yield )
+import Control.Concurrent hiding (yield)
 import Control.Concurrent.STM
 import Control.Exception
+import Control.Monad
 import qualified Data.ByteString as B
+import Data.IORef
+import Data.Serialize
+import Data.Serialize.Text ()
+import qualified Data.Text as T
+import Data.Typeable
+import Data.Word
+import GHC.Generics
+import IRC.Socket
 import Pipes
 
-import IRC.Socket
-
-data IRCMessage =
-    PrivateMessage
-    { who :: !T.Text       -- ignored when sent from client to server
-    , target :: !T.Text
-    , reply :: !T.Text
-    , content :: !T.Text }
+data IRCMessage
+  = PrivateMessage
+      { who :: !T.Text, -- ignored when sent from client to server
+        target :: !T.Text,
+        reply :: !T.Text,
+        content :: !T.Text
+      }
   | Join !T.Text
   | Part !T.Text
-  deriving ( Eq, Ord, Show, Read, Typeable, Generic )
+  deriving (Eq, Ord, Show, Read, Typeable, Generic)
 
 instance Serialize IRCMessage
 
-connectToBot :: T.Text -> Word16 -> IO ( IO (Maybe IRCMessage)
-                                       , IRCMessage -> IO () )
+connectToBot ::
+  T.Text ->
+  Word16 ->
+  IO
+    ( IO (Maybe IRCMessage),
+      IRCMessage -> IO ()
+    )
 connectToBot host port = mask $ \restore -> do
-    ref <- newIORef () -- for finalization
-
-    chan <- newTChanIO
-    mvar <- newEmptyMVar
-    tid <- forkIO $ restore $ connector chan mvar
-    sendaction <- takeMVar mvar
-    void $ mkWeakIORef ref $ killThread tid
-    return $ ( readIORef ref >> atomically (readTChan chan)
-             , \x -> readIORef ref >> sendaction x )
+  ref <- newIORef () -- for finalization
+  chan <- newTChanIO
+  mvar <- newEmptyMVar
+  tid <- forkIO $ restore $ connector chan mvar
+  sendaction <- takeMVar mvar
+  void $ mkWeakIORef ref $ killThread tid
+  return $
+    ( readIORef ref >> atomically (readTChan chan),
+      \x -> readIORef ref >> sendaction x
+    )
   where
     connector chan mvar =
-        flip finally (atomically $ writeTChan chan Nothing) $
+      flip finally (atomically $ writeTChan chan Nothing) $
         connect (T.unpack host) (show port) $ \conn addr -> do
-            putStrLn $ "Connected to " <> show addr
-            lock <- newMVar ()
-            putMVar mvar (\x -> withMVar lock $ \_ -> send conn $ encode x)
-            runEffect $
-                fromSocket conn >->
-                receiveIRCMessages >->
-                chansender chan
+          putStrLn $ "Connected to " <> show addr
+          lock <- newMVar ()
+          putMVar mvar (\x -> withMVar lock $ \_ -> send conn $ encode x)
+          runEffect $
+            fromSocket conn
+              >-> receiveIRCMessages
+              >-> chansender chan
 
     chansender chan = forever $ do
-        value <- await
-        liftIO $ atomically $ writeTChan chan $ Just value
+      value <- await
+      liftIO $ atomically $ writeTChan chan $ Just value
 
-receiveIRCMessages :: MonadIO m => Pipe B.ByteString IRCMessage m ()
+receiveIRCMessages :: (MonadIO m) => Pipe B.ByteString IRCMessage m ()
 receiveIRCMessages = decoder first_resume
   where
     first_resume :: B.ByteString -> Result IRCMessage
@@ -73,7 +80,6 @@ receiveIRCMessages = decoder first_resume
     decoder resume = await >>= rec resume
       where
         rec resume bs = case resume bs of
-            Fail _ _ -> liftIO (putStrLn "Malformed data.")
-            Partial next -> decoder next
-            Done x rest -> yield x >> rec first_resume rest
-
+          Fail _ _ -> liftIO (putStrLn "Malformed data.")
+          Partial next -> decoder next
+          Done x rest -> yield x >> rec first_resume rest
